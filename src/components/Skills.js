3aa2d4d4d15dbb5881513bc0382e3f0ff9ce939c
verissimo francis <verissimotoyin50@gmail.com>
2022-08-16 16:12:57 +0100

    create components

---
+++ src/components/Skills.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div className="Skills" />
+  );
+}
+
+export default Skills;
---
+++ src/components/navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../Images/1.png';
+import './navbar.css';
+
+function NavBar() {
+  const navItems = [
+    {
+      id: 1,
+      path: '/',
+      text: 'HOME',
+    },
+    {
+      id: 2,
+      path: '/Portfolio',
+      text: 'PORTFOLIO',
+    },
+    {
+      id: 3,
+      path: '/Skills',
+      text: 'SKILLS',
+    },
+    {
+      id: 4,
+      path: '/Contact',
+      text: 'CONTACT',
+    },
+  ];
+  return (
+    <header className="nav-head">
+      <img src={logo} className="logo" alt="logo" />
+      <nav>
+        <ul className="menu">
+          {
+                          navItems.map((navItems) => (
+                            <li key={navItems.id}>
+                              <NavLink to={navItems.path} className="menulinks">{navItems.text}</NavLink>
+                            </li>
+                          ))
+                      }
+        </ul>
+      </nav>
+
+    </header>
+  );
+}
+export default NavBar;
