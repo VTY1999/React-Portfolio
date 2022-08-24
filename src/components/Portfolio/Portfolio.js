325b19b9cda7f7f675842858b74e01fe74629444
verissimo francis <verissimotoyin50@gmail.com>
2022-08-24 16:07:40 +0100

    Add portfolio structure

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,78 @@
 import React from 'react';
+import Image1 from '../../assets/Screenshot (26).png';
+import Image2 from '../../assets/Screenshot (192).png';
+import Image3 from '../../assets/Screenshot (128).png';
+import Image4 from '../../assets/Screenshot (196).png';
+import './Portfolio.scss';
 
 function Portfolio() {
+  const Projects = [
+    {
+      id: 1,
+      title: 'CONFERENCE PAGE',
+      img: Image1,
+      description: 'This is a conference page about an art summit displaying the main program, speakers and the about section which displays past events.',
+      demo: 'https://vty1999.github.io/Conference-page/',
+      github: 'https://github.com/VTY1999/Conference-page',
+    },
+    {
+      id: 2,
+      title: 'AIR POLLUTION',
+      img: Image2,
+      description: 'This webapp tracks air pollution in more than 20 countries drawing data from an API',
+      demo: 'https://verissimofrancis-air-pollution.netlify.app/',
+      github: 'https://github.com/VTY1999/React-Capstone',
+    },
+    {
+      id: 3,
+      title: 'MATH MAGICIANS',
+      img: Image3,
+      description: 'Math magicians is a website for all mathematics fans. It is a Single Page App (SPA) that allows users to make calculations and read a random math-related quote.',
+      demo: 'https://francis-math-magicians.netlify.app/',
+      github: 'https://github.com/VTY1999/Math-Magicians',
+    },
+    {
+      id: 4,
+      title: 'Rent A Car',
+      img: Image4,
+      description: 'This is a car rental website that allows you to view a variety of cars and book them at different rates. User can see all models and model details and rerve car. Admin can add and delete car.',
+      demo: 'https://rent-car-service.netlify.app/',
+      github: 'https://github.com/Gegardus/book-a-car-frontend',
+    },
+  ];
+
   return (
-    <div className="Portfolio" />
+    <section className="Portfolio">
+      <h2 className="Portfolio-title">Portfolio</h2>
+      <hr className="h" />
+      <div className="container">
+        {Projects.map((Pro) => (
+          <article className="portfolio__item" key={Pro.id}>
+            <div className="portfolio__item-image">
+              <img src={Pro.img} alt={Pro.title} />
+            </div>
+            <div className="portfolio-content">
+              <h3>{Pro.title}</h3>
+              <p>{Pro.description}</p>
+            </div>
+            <div className="portfoli">
+              <a
+                href={Pro.github}
+                className="btn"
+              >
+                GitHub
+              </a>
+              <a
+                href={Pro.demo}
+                className="btn"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
   );
 }
 
