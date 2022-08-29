58d2c44b73692382e8831aeb2359b9e39562e762
verissimo francis <verissimotoyin50@gmail.com>
2022-08-29 16:27:54 +0100

    Add contact section to portfolio

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,9 +3,44 @@ import './Contact.scss';
 
 function Contact() {
   return (
-    <div className="Contact">
-      <p>ejjj</p>
-    </div>
+    <section className="Contact">
+      <h1 className="contact-haed">Let&apos;s talk</h1>
+      <div className="container-contact">
+        <form className="form" action="https://formspree.io/f/mayvyyqk" method="POST">
+          <input
+            type="text"
+            name="name"
+            className="fullName"
+            placeholder="Full Name"
+            maxLength="50"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            className="email"
+            placeholder="Email"
+            maxLength="50"
+            required
+          />
+          <input
+            type="text"
+            name="Subject"
+            className="subject"
+            placeholder="Subject"
+            maxLength="50"
+            required
+          />
+          <textarea
+            placeholder="Message..."
+            className="message"
+            name="message"
+            required
+          />
+          <button type="submit" className="send">Get in touch</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
