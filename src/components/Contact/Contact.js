09604b514da026fb361d545d0fe155fe0fd57aad
verissimo francis <verissimotoyin50@gmail.com>
2022-09-01 14:33:43 +0100

    Add final touch to form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaAngellist } from 'react-icons/fa';
 import './Contact.scss';
 
 function Contact() {
@@ -7,22 +8,24 @@ function Contact() {
       <h1 className="contact-haed">Let&apos;s talk</h1>
       <div className="container-contact">
         <form className="form" action="https://formspree.io/f/mayvyyqk" method="POST">
-          <input
-            type="text"
-            name="name"
-            className="fullName"
-            placeholder="Full Name"
-            maxLength="50"
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            className="email"
-            placeholder="Email"
-            maxLength="50"
-            required
-          />
+          <div className="input-row">
+            <input
+              type="text"
+              name="name"
+              className="fullName"
+              placeholder="Full Name"
+              maxLength="50"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              className="email"
+              placeholder="Email"
+              maxLength="50"
+              required
+            />
+          </div>
           <input
             type="text"
             name="Subject"
@@ -39,6 +42,11 @@ function Contact() {
           />
           <button type="submit" className="send">Get in touch</button>
         </form>
+        <div className="form-social">
+          <a href="https://www.linkedin.com/in/francis-o-verissimo/" alt="LinkedIn"><FaLinkedin /></a>
+          <a href="https://github.com/VTY1999" alt="GitHub"><FaGithub /></a>
+          <a href="https://angel.co/u/verissimo-francis" alt="Angellist"><FaAngellist /></a>
+        </div>
       </div>
     </section>
   );
