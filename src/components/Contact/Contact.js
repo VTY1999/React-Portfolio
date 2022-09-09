7defebff6d905aee5e1ea911a4a15b62813559e7
verissimo francis <verissimotoyin50@gmail.com>
2022-09-09 12:04:56 +0100

    Fix Contact form Bug

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import { FaLinkedin, FaGithub, FaAngellist } from 'react-icons/fa';
 import './Contact.scss';
 
 function Contact() {
+  const [state, handleSubmit] = useForm('xkneopej');
+  if (state.succeeded) {
+    return <p className="success-msg">Thanks for your message. I&apos;m unavailable right now, but will respond as soon as possible! 😜</p>;
+  }
+
   return (
     <section className="Contact">
       <h1 className="contact-haed">Let&apos;s talk</h1>
       <div className="container-contact">
-        <form className="form" action="https://formspree.io/f/mayvyyqk" method="POST">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="input-row">
             <input
               type="text"
@@ -25,6 +31,11 @@ function Contact() {
               maxLength="50"
               required
             />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
           <input
             type="text"
@@ -40,7 +51,13 @@ function Contact() {
             name="message"
             required
           />
-          <button type="submit" className="send">Get in touch</button>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+
+          <button className="form-btn" type="submit" disabled={state.submitting}>Get in touch</button>
         </form>
         <div className="form-social">
           <a href="https://www.linkedin.com/in/francis-o-verissimo/" alt="LinkedIn"><FaLinkedin /></a>
